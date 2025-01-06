a3567fe26a28626f3acf26d2045d286870abfb82
Vishwaraj Rathore <53338624+Vishwaraj-Rathore@users.noreply.github.com>
2025-01-06 17:03:23 +0530

    small change

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
   return (
-    <div className="h-[calc(100vh-75px)] md:h-[calc(100vh-85px)] bg-black">
-      <div className=""></div>
+    <div className="h-[calc(100vh-75px)] md:h-[calc(100vh-85px)] flex items-center justify-center">
+      <div className="bg-[#27374D] h-[calc(100%-50px)] w-[calc(100%-50px)] rounded-2xl"></div>
     </div>
   );
 }
