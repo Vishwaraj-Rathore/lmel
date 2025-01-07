75840d2b37810a82dd2bc7982c108a02b6fa49a9
Vishwaraj Rathore <53338624+Vishwaraj-Rathore@users.noreply.github.com>
2025-01-07 12:31:41 +0530

    minor change

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <div className="h-[calc(100vh-75px)] md:h-[calc(100vh-85px)] flex items-center justify-center">
       <div className="bg-[#526D82] h-[calc(100%-40px)] w-[calc(100%-40px)] md:h-[calc(100%-100px)] md:w-[calc(100%-100px)] rounded-[10px] flex flex-col items-center">
-        <p className="text-[#DDE6ED] mt-[150px] text-[24px]">
+        <p className="text-[#DDE6ED] mt-[190px] text-[24px]">
           Last Mile Enterprises Ltd.
         </p>
         <p className="text-black mt-[30px] text-[50px]">
