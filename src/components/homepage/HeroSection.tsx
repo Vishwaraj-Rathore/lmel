4e71b1bd613f299ed2e8b5da4d3cc4ab4079fe6e
Vishwaraj Rathore <53338624+Vishwaraj-Rathore@users.noreply.github.com>
2025-01-08 09:56:18 +0530

    made content in herosection centered

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -1,11 +1,9 @@
 export default function HeroSection() {
   return (
     <div className="h-[calc(100vh-75px)] md:h-[calc(100vh-85px)] flex items-center justify-center">
-      <div className="bg-[#526D82] h-[calc(100%-40px)] w-[calc(100%-40px)] md:h-[calc(100%-100px)] md:w-[calc(100%-100px)] rounded-[10px] flex flex-col items-center">
-        <p className="text-[#DDE6ED] mt-[190px] text-[24px]">
-          Last Mile Enterprises Ltd.
-        </p>
-        <p className="text-black mt-[30px] text-[50px]">
+      <div className="bg-[#526D82] h-[calc(100%-40px)] w-[calc(100%-40px)] md:h-[calc(100%-100px)] md:w-[calc(100%-100px)] rounded-[10px] flex flex-col items-center justify-center">
+        <p className="text-[#DDE6ED] text-[24px]">Last Mile Enterprises Ltd.</p>
+        <p className="text-black text-[50px]">
           Ingenious
           <br />
           Intent,
